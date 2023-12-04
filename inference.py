bea61346aed4aac9f946e2367999a2a68bd6f8f2
TrickmanOff <egorkadv2002@gmail.com>
2023-12-04 09:17:35 +0300

    add inference

--- inference.py
+++ inference.py
@@ -1,7 +1,13 @@
+import json
 import shutil
 from pathlib import Path
 
-from lib.utils import download_file
+import torch
+
+from lib.encoder import BPETextEncoder
+from lib.model import SimpleTransformer
+from lib.text_generator import RandomGenerator
+from lib.utils import download_file, get_device
 
 
 URL_LINKS = {
@@ -29,6 +35,24 @@ def main():
         if not encoder_arch_path.exists():
             download_file(URL_LINKS['encoder'], encoder_dirpath, 'encoder.zip')
         shutil.unpack_archive(encoder_arch_path, encoder_dirpath)
+
+    encoder = BPETextEncoder(name='tiny_stories_encoder_4k',
+                             encoder_dirpath=encoder_dirpath)
+
+    device = get_device()
+    model_config = json.load(open(Path(__file__).parent / 'model_configs/model1.json', 'r'))
+    model = SimpleTransformer(vocab_size=encoder.vocab_size, max_len=512, **model_config)
+    state_dict = torch.load(checkpoint_filepath, map_location=device)
+    model.load_state_dict(state_dict['model'])
+    print('Model weights loaded')
+
+    text_generator = RandomGenerator(temp=0.2, top_k=35, model=model, encoder=encoder, max_len=512)
+    while True:
+        prompt = input('Enter a prompt')
+        print('Processing...')
+        result = text_generator.generate_text(prompt)
+        print(result)
+        print('='*10)
 
 
 if __name__ == '__main__':
